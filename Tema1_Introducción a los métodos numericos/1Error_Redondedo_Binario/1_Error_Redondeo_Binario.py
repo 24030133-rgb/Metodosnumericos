332610b7102fd1ad5b111e2974195d5c8db1b351
24030133-rgb <24030133@itesa.edu.mx>
2026-04-25 12:57:29 -0600

    Agregar métodos numéricos

--- Tema1_Introducción a los métodos numericos/1Error_Redondedo_Binario/1_Error_Redondeo_Binario.py
+++
@@ -1,9 +0,0 @@
-# Error de Redondeo Binario
-
-a = 0.1
-b = 0.2
-resultado = a + b
-
-print(f"0.1 + 0.2 = {resultado:.4f}")
-print(f"¿Es igual a 0.3? {round(resultado, 4) == round(0.3, 4)}")
-print(f"Error: {abs(0.3 - resultado):.4f}")
